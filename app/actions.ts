0ca5cbb8dffa7f1964abbf841eb03412d73cc475
Daniel_MT.dev <69852644+Danielmeza2599@users.noreply.github.com>
2025-12-09 19:16:00 -0600

    functions that run on the server but are called from the client.

---
+++ app/actions.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import { createClient } from '@/utils/supabase/server'
+import { revalidatePath } from 'next/cache'
+
+export async function addTodo(formData: FormData) {
+  const supabase = await createClient()
+  
+  // the current user is obtained to assign the task
+  const { data: { user } } = await supabase.auth.getUser()
+  if (!user) return;
+
+  const title = formData.get('title') as string
+
+  await supabase.from('todos').insert({ 
+    title, 
+    user_id: user.id 
+  })
+
+  revalidatePath('/') // UI is refreshed
+}
+
+export async function updateTodoStatus(id: number, is_complete: boolean) {
+  const supabase = await createClient()
+  await supabase.from('todos').update({ is_complete }).eq('id', id)
+  revalidatePath('/')
+}
+
+export async function deleteTodo(id: number) {
+  const supabase = await createClient()
+  await supabase.from('todos').delete().eq('id', id)
+  revalidatePath('/')
+}
+
+export async function updateTodoTitle(formData: FormData) {
+  const supabase = await createClient()
+  const id = formData.get('id')
+  const title = formData.get('title')
+  
+  await supabase.from('todos').update({ title }).eq('id', id)
+  revalidatePath('/')
+}
+
+export async function signOut() {
+  const supabase = await createClient()
+  await supabase.auth.signOut()
+  return { success: true }
+}
